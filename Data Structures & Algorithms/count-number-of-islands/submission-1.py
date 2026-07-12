35c419d585cead98158bf3434177589f00f46d10
Kshitij Desai <79054156+kshitijdesai99@users.noreply.github.com>
2026-07-12 18:11:51 +0930

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,42 @@
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        output = 0
+        if not grid:
+            return output
+
+        n_rows, n_cols = len(grid), len(grid[0])
+
+        visited = set()
+
+        def bfs(row,col):
+            q = deque([(row,col)])
+            while q:
+                rowz, colz = q.popleft()
+                
+                up, down, left, right = (1,0),(-1,0),(0,-1),(0,1)
+
+                for dr, dc in (up, down, left, right):
+                    row = rowz + dr
+                    col = colz + dc
+                    if ((0<=row<n_rows) and
+                        (0<=col<n_cols) and
+                        grid[row][col] == "1" and
+                        (row,col) not in visited):
+                        visited.add((row,col))
+                        q.append((row,col))
+
+         
+        for row in range(n_rows):
+            for col in range(n_cols):
+                if((row,col) not in visited and 
+                grid[row][col] == "1"):
+                    print(row,col)
+                    visited.add((row,col))
+                    output+=1
+                    bfs(row,col)
+
+        # Time complexity - O(m*n)
+        # Space complexity - O(m*n)
+        
+        return output

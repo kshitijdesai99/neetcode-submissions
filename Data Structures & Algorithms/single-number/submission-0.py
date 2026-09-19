3908346b1c28a350757c049c85a503092f87826a
Kshitij Desai <79054156+kshitijdesai99@users.noreply.github.com>
2026-09-20 00:59:38 +0930

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        output = 0
+        for i in nums:
+            output^=i
+        return output
+        # Time - O(n)
+        # Space - O(1)
